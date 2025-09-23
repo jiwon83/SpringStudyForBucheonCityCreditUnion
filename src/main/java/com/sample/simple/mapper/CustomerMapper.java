f2aea32a6b05f008562eb56d97dc35a5a7247ca1
jiwon83 <gkswldnj0803@gmail.com>
2025-09-23 13:40:22 +0900

    Initial commit

---
+++ src/main/java/com/sample/simple/mapper/CustomerMapper.java
@@ -0,0 +1,24 @@
+package com.sample.simple.mapper;
+
+import com.sample.simple.domain.Customer;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CustomerMapper {
+    // 고객 전체 조회
+    List<Customer> findAll();
+
+    // ID로 단일 조회
+    Customer findById(Integer customerId);
+
+    // 고객 등록
+    int insert(Customer customer);
+
+    // 고객 수정
+    int update(Customer customer);
+
+    // 고객 삭제
+    int delete(Integer customerId);
+}
